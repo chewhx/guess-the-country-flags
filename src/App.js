61f979fc79aabca368683c1a671bd7ff0872a886
chewhx <chewhx@icloud.com>
2021-06-16 16:44:10 +0800

    for deploy on netlify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
